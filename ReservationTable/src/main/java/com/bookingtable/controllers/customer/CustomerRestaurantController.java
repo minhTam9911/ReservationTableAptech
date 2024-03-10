08288eab9b04da0ee30b6f8096d0c534f5b59496
vcecp24@gmail.com <vcecp24@gmail.com>
2024-03-10 11:10:12 +0700

    no message

--- ReservationTable/src/main/java/com/bookingtable/controllers/customer/CustomerRestaurantController.java
+++ ReservationTable/src/main/java/com/bookingtable/controllers/customer/CustomerRestaurantController.java
@@ -7,17 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bookingtable.servicies.IRestaurantService;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
-@RequestMapping("restaurant")
+@RequestMapping("customer/restaurant")
 public class CustomerRestaurantController {
 
 	@Autowired
 	private IRestaurantService restaurantService;
 	
 	
-	@GetMapping("index")
+	@GetMapping({"index","/",""})
 	public String index(Model model) {
+		String requestURI = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getRequestURI();
+		model.addAttribute("requestURI", requestURI);
 		var data = restaurantService.getAllRestaurants();
 		model.addAttribute("data", data);
 		return "customer/restaurant/index";
