340aa0ea1b27979fee66417bcd1054cfe05ad48d
vcecp24@gmail.com <vcecp24@gmail.com>
2024-03-12 16:55:15 +0700

    no message

--- ReservationTable/src/main/java/com/bookingtable/controllers/customer/CustomerInvoiceController.java
+++ ReservationTable/src/main/java/com/bookingtable/controllers/customer/CustomerInvoiceController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +18,7 @@ public class CustomerInvoiceController {
 	@Autowired
 	private IInvoiceService iInvoiceService;
 
-	@PostMapping("index")
+	@GetMapping("index")
 	public String index(Model model, Principal principal) {
 		if(principal.getName() !=null || !principal.getName().isEmpty()) {
 			model.addAttribute("data",iInvoiceService.getByCustomer(principal.getName()));
