1b10f42a7d4df4f3c04b9f210480aaa551e88fc9
vcecp24@gmail.com <vcecp24@gmail.com>
2024-03-13 21:37:32 +0700

    # collection display

--- ReservationTable/src/main/java/com/bookingtable/controllers/customer/CollectionController.java
+++ ReservationTable/src/main/java/com/bookingtable/controllers/customer/CollectionController.java
@@ -1,7 +1,12 @@
 package com.bookingtable.controllers.customer;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
+import com.bookingtable.dtos.ImageDto;
+import com.bookingtable.dtos.RestaurantDto;
+import com.bookingtable.servicies.IImageService;
+import com.bookingtable.servicies.IRestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,23 +24,36 @@ public class CollectionController {
 
 	@Autowired
 	private ICollectionService collectionService;
-	
+	@Autowired
+	private IRestaurantService restaurantService;
+	@Autowired
+	private IImageService imageService;
 	@GetMapping("index")
 	public String index(Model model, Principal principal) {
 		if(principal !=null) {
-			model.addAttribute("data",collectionService.getByCustomer(principal.getName()));
+			var collections = collectionService.getByCustomer(principal.getName());
+			var restaurants = new ArrayList<RestaurantDto>(); // Tạo danh sách để chứa tất cả các nhà hàng
+
+			for (var collection : collections){
+				var restaurant = restaurantService.getRestaurantById(collection.getRestaurant().getId());
+				var imageList = new ArrayList<ImageDto>();
+				var images = imageService.getImagesByRestaurantId(collection.getRestaurant().getId());
+				for (var i: images){
+					imageList.add(i);
+					restaurant.setImagesDto(imageList);
+				}
+				restaurants.add(restaurant); // Thêm nhà hàng vào danh sách
+			}
+			model.addAttribute("restaurants",restaurants);
 			return "customer/collection/index";
 		}
 		return "account/accessDined";
 	}
 	
-//	@GetMapping("delete/{id}")
-//	public String delete(Model model,@PathVariable("id") Integer id, Principal principal) {
-//		if(principal !=null) {
-//			collectionService.delete(id, principal.getName());
-//			return "redirect:/customer/collection/index";
-//		}
-//		return "account/accessDined";
-//	}
+	@GetMapping("delete/{id}")
+	public String delete(Model model,@PathVariable("id") Integer id) {
+			collectionService.delete(id);
+			return "redirect:/customer/collection/index";
+	}
 	
 }
